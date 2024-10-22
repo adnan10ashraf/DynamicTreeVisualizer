4c7a5c3c1cfc8666bf2323c93a487b301ca8124b
adnanashraf <adnan20ashraf@gmail.com>
2024-10-22 11:50:06 -0700

    update git ignore and implement parent pre-processing

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ def process_data():
     df = pd.read_csv("people.csv")
     for _, row in df.iterrows():
         name = f'{row['first_name']} {row['last_name']}'
-        people[name] = row['id'] 
+        people[row['id']] = name
+    df2 = pd.read_csv('parents.csv')
+    for _, row in df2.iterrows():
+        parents[row['child_id']] = (row['father_id'], row['mother_id'])
 
 
 
 if __name__ == "__main__":
     process_data()
     print(people)
+    print(parents)
